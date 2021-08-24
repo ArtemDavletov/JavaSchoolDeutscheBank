0e65e9ce5b3f2b490617f6e45145bc6b46a3fb45
Artem_Davletov <Tomas-Cooper@mail.ru>
2021-08-24 15:58:33 +0300

    Rewrite task with stream

--- src/main/java/com/db/lectures/lecture7/taskBeforeLanch/Seniority.java
+++ src/main/java/com/db/lectures/lecture7/taskBeforeLanch/Seniority.java
@@ -1,14 +1,24 @@
 package com.db.lectures.lecture7.taskBeforeLanch;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+@RequiredArgsConstructor
+@Getter
 public enum Seniority {
-    JUN, MIDDLE, SENIOR;
+    JUN(10), MIDDLE(20), SENIOR(Integer.MAX_VALUE);
     //jun <10
     //middle >10  <20
     //senior >20
+    private final int value;
 
     // you can add any code you want but try to avoid using if block
     public static Seniority defineSeniority(int salary) {
-        // Мне не нравится это решение, но это самое простое, что пришло в голову :)
-        return salary < 10 ? JUN : (salary < 20 ? MIDDLE : SENIOR);
+//        return salary < 10 ? JUN : (salary < 20 ? MIDDLE : SENIOR);
+        return Arrays.stream(values()).filter(v -> v.value >= salary).findFirst().get();
     }
 }
